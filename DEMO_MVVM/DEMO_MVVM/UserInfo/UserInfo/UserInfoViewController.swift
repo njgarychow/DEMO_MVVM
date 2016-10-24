45061073bd419ee5d94c9a7a19dc4c00a278afa0
jqzhou <Jinqiang.Zhou@wnco.com>
2016-10-24 17:12:35 +0800

    trigger the segue perform when click the retrive more button on user info page.

--- DEMO_MVVM/DEMO_MVVM/UserInfo/UserInfo/UserInfoViewController.swift
+++ DEMO_MVVM/DEMO_MVVM/UserInfo/UserInfo/UserInfoViewController.swift
@@ -4,6 +4,8 @@ class UserInfoViewController: UIViewController {
     @IBOutlet weak var userInfoLabel: UILabel!
     @IBOutlet weak var retriveMoreButton: UIButton!
     
+    @IBOutlet weak var loadingStatusLabel: UILabel!
+    
     var viewModel: UserInfoViewModel!
     
     override func viewDidLoad() {
@@ -19,7 +21,29 @@ class UserInfoViewController: UIViewController {
     }
     
     @objc func retriveMoreButtonClicked() {
-        
+        loadingStatusLabel.text = "Starting loading"
+        weak var weakSelf = self
+        viewModel.retriveMoreUserInfoDetails() { userInfoDetailsViewModel in
+            if let strongSelf = weakSelf {
+                strongSelf.loadingStatusLabel.text = ""
+                strongSelf.displayUserInfoDetailsPage(viewModel: userInfoDetailsViewModel)
+            }
+        }
+    }
+    
+    func displayUserInfoDetailsPage(viewModel: UserInfoDetailsViewModel) {
+        self.performSegue(withIdentifier: "DisplayUserInfoDetailsSegue", sender: viewModel)
+    }
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        switch segue.identifier {
+        case .some("DisplayUserInfoDetailsSegue"):
+            let destination = segue.destination as! UserInfoDetailsViewController
+            let viewModel = sender as! UserInfoDetailsViewModel
+            destination.viewModel = viewModel
+        default:
+            break
+        }
     }
     
 }
